cb90dd198767c72e51acd5250bae33f157c53632
KCHENPENGFEI <chen_gs@zju.edu.cn>
2019-04-02 20:38:08 +0800

    增加了一些注释

--- zjubca.donate.cpp
+++ zjubca.donate.cpp
@@ -2,15 +2,19 @@
 
 void Donate::start()
 {
+    //确保只有本合约才有权力执行start这个action
     require_auth(_self);
 
     name recipient_account = name("donate");
     name foundation_account = name("zjubca");
 
+    //查找recipient表中是否已经存在donate这个字段
     auto existing1 = recipient.find(recipient_account.value);
+    //查找foundation表中是否已经存在zjubca这个字段
     auto existing2 = foundation.find(foundation_account.value);
     if (existing1 == recipient.end())
     {
+        //如果没找到，插入初始化数据
         recipient.emplace(_self, [&](auto &new_recipient) {
             new_recipient.recipient_name = recipient_account;
             new_recipient.ZJUBCA_amount = asset(0, symbol("ZJUBCA", 4));
@@ -19,6 +23,7 @@ void Donate::start()
     }
     if (existing2 == foundation.end())
     {
+        //如果没有找到，插入初始化数据
         foundation.emplace(_self, [&](auto &new_foundation) {
             new_foundation.foundation_name = foundation_account;
             new_foundation.ZJUBCA_amount = asset(0, symbol("ZJUBCA", 4));
@@ -29,10 +34,15 @@ void Donate::start()
 
 void Donate::donatezjubca(name from, name to, asset quantity, string memo)
 {
+    //建议先看apply函数(代码最后)中的注释代码
+
+    //检查memo是否是"donatezjubca"，防止其他转账操作而进入本action
     if (memo == "donatezjubca")
     {
+        //进行一些assert操作
         eosio_assert(from != to, "cannot transfer to self");
         require_auth(from);
+        //接收ZJUBCA的地址只能是本合约
         eosio_assert(to == _self, "only can donate to the contract");
 
         auto sym = quantity.symbol;
@@ -44,9 +54,11 @@ void Donate::donatezjubca(name from, name to, asset quantity, string memo)
 
         eosio_assert(memo.size() <= 256, "memo has more than 256 bytes");
 
+        //先从donators表中查询捐赠者
         auto existing1 = donator.find(from.value);
         if (existing1 == donator.end())
         {
+            //不存在就在新增一个用户
             donator.emplace(_self, [&](auto &new_donator) {
                 new_donator.donator_name = from;
                 new_donator.ZJUBCA_amount = quantity;
@@ -57,6 +69,7 @@ void Donate::donatezjubca(name from, name to, asset quantity, string memo)
         }
         else
         {
+            //若已经存在，则修改用户数据
             donator.modify(existing1, same_payer, [&](auto &content) {
                 content.ZJUBCA_amount += quantity;
             });
@@ -65,6 +78,7 @@ void Donate::donatezjubca(name from, name to, asset quantity, string memo)
         auto existing2 = recipient.find(to.value);
         if (existing2 != recipient.end())
         {
+            //修改recipient表中数据
             recipient.modify(existing2, same_payer, [&](auto &content) {
                 content.ZJUBCA_amount += quantity;
             });
@@ -74,6 +88,7 @@ void Donate::donatezjubca(name from, name to, asset quantity, string memo)
 
 void Donate::donateeos(name from, name to, asset quantity, string memo)
 {
+    //和doantezjubca逻辑类似
     if (memo == "donateeos")
     {
         eosio_assert(from != to, "cannot transfer to self");
@@ -119,6 +134,7 @@ void Donate::donateeos(name from, name to, asset quantity, string memo)
 
 void Donate::end()
 {
+    //删除表操作
     auto donator_begin_it = donator.begin();
     while (donator_begin_it != donator.end())
     {
@@ -140,6 +156,7 @@ void Donate::end()
 
 void Donate::sendtofound(name from, name to, string memo)
 {
+    //assert
     eosio_assert(from != to, "cannot transfer to self");
     require_auth(from);
     eosio_assert(from == _self, "invalid transfer account");
@@ -153,6 +170,7 @@ void Donate::sendtofound(name from, name to, string memo)
         const auto &st1 = *existing1;
         if (st1.ZJUBCA_amount >= ZJUBCA_quantity)
         {
+            //查表并且检查合约地址中token数量是否发超过一定限额
             recipient.modify(st1, from, [&](auto &content) {
                 content.ZJUBCA_amount -= ZJUBCA_quantity;
             });
@@ -174,6 +192,7 @@ void Donate::sendtofound(name from, name to, string memo)
         }
         if (st1.EOS_amount >= EOS_quantity)
         {
+            //逻辑同上
             recipient.modify(st1, from, [&](auto &content) {
                 content.EOS_amount -= EOS_quantity;
             });
@@ -186,6 +205,7 @@ void Donate::sendtofound(name from, name to, string memo)
                     content.EOS_amount += EOS_quantity;
                 });
             }
+            //此处由于是合约将自己的token进行转出，所以可以直接使用inline action操作，不存在安全性问题
             action(
                 permission_level{from, "active"_n},
                 "eosio.token"_n,
@@ -199,14 +219,19 @@ void Donate::sendtofound(name from, name to, string memo)
 // EOSIO_DISPATCH(Donate, (start)(donatezjubca)(donateeos)(sendtofound)(end))
 extern "C"
 {
+    //apply函数是所有EOS合约的入口
+    //在EOS中，合约之间是可以相互调用的，假设A合约在B这个action中调用了C合约，那么receiver就是C合约，code就是A合约，action就是B这个action
     void apply(uint64_t receiver, uint64_t code, uint64_t action)
     {
+        //判断是action的入口是本合约还是别的合约
+        //如果调用方和被调用方式相同的，并且不是从transfer这个action进入的
         if (code == receiver && action != "transfer"_n.value)
         {
             switch (action)
             {
+            //进入不同的action
             case "start"_n.value:
-                execute_action(name(receiver), name(code), &Donate::start);
+                execute_action(name(receiver), name(code), &Donate::start);    //使用execute_action进入相应的action
                 break;
             case "end"_n.value:
                 execute_action(name(receiver), name(code), &Donate::end);
@@ -218,13 +243,16 @@ extern "C"
                 break;
             }
         }
+        //code == zjubca.token和code == eosio.token意为调用本合约的合约为zjubca.token和eosio.token，并且是从transfer这个action进入的
         else if ((code == "zjubca.token"_n.value || code == "eosio.token"_n.value) && action == "transfer"_n.value)
         {
             switch (code)
             {
+            //如果是zjubca.token进入本合约，进行donatezjubca这个action
             case "zjubca.token"_n.value:
                 execute_action(name(receiver), name(receiver), &Donate::donatezjubca);
                 break;
+            //如果是eosio.token进入本合约，进行donateeos这个action
             case "eosio.token"_n.value:
                 execute_action(name(receiver), name(receiver), &Donate::donateeos);
                 break;
--- zjubca.donate.hpp
+++ zjubca.donate.hpp
@@ -1,10 +1,13 @@
 #pragma once
 
+//包含需要的头文件
 #include <eosiolib/eosio.hpp>
 #include <eosiolib/asset.hpp>
 #include <eosiolib/symbol.hpp>
 #include <string>
 
+//宏定义 MAX_ZJUBCA_QUANTITY用于检查合约账户中ZJUBCA数量是否超过10000.0000 ZJUBCA
+//MAX_EOS_QUANTITY 用于检查合约账户中EOS数量是否超过1000.000 EOS  (此处ZJUBCA精度是4位，EOS是三位)
 #define MAX_ZJUBCA_QUANTITY 100000000
 #define MAX_EOS_QUANTITY 1000000
 
@@ -15,27 +18,33 @@ namespace eosiosystem {
 class system_contract;
 }
 
+//定义zjubca.donate合约这个类
 class [[eosio::contract("zjubca.donate")]] Donate : public contract
 {
 private:
+    //定义donator表的结构，该表用于存储捐赠者的基本信息
+    //主键是donator_name，类型是name，ZJUBCA_amount和EOS_amount用于保存该用户总共捐赠的ZJUBCA和EOS数量，类型是assset
     // @abi table doantor
     struct [[eosio::table]] donator {
         name donator_name;
         asset ZJUBCA_amount;
         asset EOS_amount;
 
-        auto primary_key() const {return donator_name.value;}
+        auto primary_key() const {return donator_name.value;}  //声明主键是donator_name.value
     };
 
+    //定义recipient表的结构，该表用于存储该合约的基本信息
+    //主键是recipient_name，类型是name，(其实这个数据只有本合约)ZJUBCA_amount和EOS_amount用于保存本合约总共收到捐赠的ZJUBCA和EOS数量，类型是assset
     // @abi table recipient
     struct [[eosio::table]] recipient {
         name recipient_name;
         asset ZJUBCA_amount;
         asset EOS_amount;
 
-        auto primary_key() const {return recipient_name.value;}
+        auto primary_key() const {return recipient_name.value;}  ////声明主键是recipient_name.value
     };
 
+    //定义foundation表的结构，该表用于存储基金会的基本信息，和recipient类似
     // @abi table foundation
     struct [[eosio::table]] foundation {
         name foundation_name;
@@ -46,23 +55,26 @@ private:
     };
     
 
+    //mutil_index<"donators"_n, donator>是mutil_index的初始化, (_donators是一个类)现在我们可以拿着_donators去实例化一张表
     typedef multi_index<"donators"_n, donator> _donators;
     typedef multi_index<"recipients"_n, recipient> _recipients;
     typedef multi_index<"foundation"_n, foundation> _foundation;
 
-    _donators donator;
-    _recipients recipient;
-    _foundation foundation;
+    _donators donator;      //实例化donator这张表
+    _recipients recipient;     //实例化recipent这张表
+    _foundation foundation;      //实例化foundation这张表
 
 public:
     using contract::contract;
 
+    //构造函数以及三张表的初始化
     Donate(name receiver, name code, datastream<const char*> ds):contract(receiver, code, ds), 
         donator(receiver, code.value), recipient(receiver, code.value), foundation(receiver, code.value){}
 
-    [[eosio::action]] void start();
-    [[eosio::action]] void end();
-    [[eosio::action]] void donatezjubca(name from, name to, asset quantity, string memo);
-    [[eosio::action]] void donateeos(name from, name to, asset quantity, string memo);
-    [[eosio::action]] void sendtofound(name from, name to, string memo);
+    //声明各个action
+    [[eosio::action]] void start();     //合约开始的时候要执行start， 向recipient和foundation这两张表中插入数据
+    [[eosio::action]] void end();       //删除表，需要时可以执行
+    [[eosio::action]] void donatezjubca(name from, name to, asset quantity, string memo);    //用户捐赠ZJUBCA给合约
+    [[eosio::action]] void donateeos(name from, name to, asset quantity, string memo);       //用户捐赠EOS给合约
+    [[eosio::action]] void sendtofound(name from, name to, string memo);     //检查合约账户中EOS数量是否超过1000.000 EOS，以及ZJUBCA数量是否超过10000.0000 ZJUBCA，然后将其转到基金会
 };
